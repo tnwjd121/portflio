d7b202376afec481e93e393689e5e39f39968e5f
tnwjd121 <144767705+tnwjd121@users.noreply.github.com>
2025-01-22 22:59:52 +0900

    프로젝트 생성

---
+++ src/components/Footer.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import { MdOutlineMail } from "react-icons/md";
+import { BsGithub } from "react-icons/bs";
+import { FaInstagram } from "react-icons/fa6";
+import "../css/footer.css"
+
+export default function Footer() {
+  return (
+    <div>
+      <div id='icon-list'>
+        <div><MdOutlineMail /></div>
+        <div><BsGithub /></div>
+        <div><FaInstagram /></div>
+      </div>
+      <div>
+        <p>© 2025 baek sujung. All rights reserved.</p>
+        <p>Thank you for visiting my portfolio!</p>
+      </div>
+    </div>
+  )
+}
---
+++ src/components/Header.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import '../css/header.css'
+
+export default function Header() {
+  return (
+    <div id='header-body'>
+      <div id='header-left'>
+        <div>Logo</div>
+      </div>
+      <div id='header-right'>
+        <div>Stacks</div>
+        <div>Project</div>
+        <div>Study</div>
+      </div>
+    </div>
+  )
+}
